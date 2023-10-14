59a076b8a5c86235db52d10d96e6a91ebfecf747
mahmoud ashraf <mahmoudashraf11179@gmail.com>
2023-10-14 15:51:38 +0300

    fix tailwind

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,1 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export * from 'tailwind-config/postcss.config';
--- packages/web-ui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-default-export */
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
