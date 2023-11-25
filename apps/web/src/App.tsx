2f8f31af8887a002b368f8f1961b27e9e6df515d
Pierre Tamer <Princelion28.pt@gmail.com>
2023-11-25 17:19:41 +0200

    initial configuration

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,7 @@
-import { CountLogic } from "@atomic/frontend-logic";
-import { Button, CountDisplay } from "@atomic/web-ui";
+import {LoginForm } from "@atomic/web-ui";
 
 function App() {
-  return (
-    <CountLogic>
-      {(count, increment, decrement) => (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black text-white">
-          <CountDisplay count={count} />
-          <Button onPress={increment} title="+1" />
-          <Button onPress={decrement} title="-1" />
-        </div>
-      )}
-    </CountLogic>
-  );
+  return <LoginForm />;
 }
 
 export default App;
