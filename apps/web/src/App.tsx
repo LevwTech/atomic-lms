943dee890b0f4f0271e29a51bbc7c02fdd087e08
Pierre Tamer <Princelion28.pt@gmail.com>
2023-11-27 14:07:00 +0200

    Added Logo Background

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,33 +1,11 @@
 import { Carousal, LoginForm } from "@atomic/web-ui";
+import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
-  const images = [
-    {
-      src: "./BUE.jpg",
-      alt: "image1",
-    },
-    {
-      src: "./BUE2.jpg",
-      alt: "image2",
-    },
-    {
-      src: "./BUE3.jpg",
-      alt: "image3",
-    },
-  ];
+
 
   return (
-    <div className="flex items-center justify-end h-screen bg-[#EFF2FB]">
-      <img
-        src="./logo-banner.svg"
-        className="absolute top-0 left-[7.5%] w-16 "
-      />
-      {/* <img src="./LogoBackground.svg" alt="" className=" z-100" /> */}
-      <LoginForm logo="./BUE_Logo.svg" />
-      <div className="w-[50%] h-full py-[70px] pr-[70px]">
-        <Carousal images={images} />
-      </div>
-    </div>
+    <LoginPage />
   );
 }
 
---
+++ apps/web/src/LoginPage/LoginPage.tsx
@@ -0,0 +1,33 @@
+import { Carousal, LoginForm } from "@atomic/web-ui";
+import styles from "./LoginPage.module.css";
+function LoginPage() {
+  const images = [
+    {
+      src: "./BUE.jpg",
+      alt: "image1",
+    },
+    {
+      src: "./BUE2.jpg",
+      alt: "image2",
+    },
+    {
+      src: "./BUE3.jpg",
+      alt: "image3",
+    },
+  ];
+  return (
+    <div className={styles.LoginPage}>
+      <img
+        src="./logo-banner.svg"
+        className="absolute top-0 left-[7.5%] w-16 "
+      />
+
+      <LoginForm logo="./BUE_Logo.svg" />
+      <div className="w-[50%] h-full py-[70px] pr-[70px]">
+        <Carousal images={images} />
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
