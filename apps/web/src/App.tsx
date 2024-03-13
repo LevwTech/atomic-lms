347c74ebee851516092a54ae1f06e66281fc8498
Ahmed Mohamed Fady <fadiahmed298@gmail.com>
2024-03-14 00:33:56 +0200

    Update routes and UI components

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,19 @@
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import CoursesPage from "./Pages/Courses/CoursesPage";
 import { Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./utils/ProtectedRoute";
+import Announcements from "./Pages/Announcements";
+import Grades from "./Pages/Grades";
+import SingleCoursePage from "./Pages/Courses/SingleCoursePage";
+
 function App() {
   return (
-    // <Routes>
-    //   <Route
-    //     path="/"
-    //     element={
-    //       <ProtectedRoute>
-    <CoursesPage />
-    //       </ProtectedRoute>
-    //     }
-    //   />
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/courses" element={<CoursesPage />} />
+      <Route path="/announcements" element={<Announcements />} />
+      <Route path="/grades" element={<Grades />} />
+      <Route path="/courseId" element={<SingleCoursePage />} />
+    </Routes>
   );
 }
 
